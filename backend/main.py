e2ec657eb6cd8fb9742956b5158d9acc28287044
pushkin <maks.bur2809@gmail.com>
2022-01-13 12:06:02 +0300

    Задача на бэке

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
+import math
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def is_square_part(area: int):
+    length = math.sqrt(area)
+    if length - int(length) != 0:
+        return False
+    else:
+        return True
+
+
+@app.get("/square")
+async def root(area: int):
+    return {"result": is_square_part(area)}
+
+
+
