72e1b6a667e20a4852d2e3b9aea35babf0bf4589
evgestrogan <evgestrogan@gmail.com>
2022-01-13 16:06:44 +0300

    [Frontend] - create form

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card width="600" style="position:absolute; left: 50%; top: 50%; transform: translate(-50%, -50%);">
-    <v-card-title></v-card-title>
-    <v-card-subtitle></v-card-subtitle>
+    <v-card-title>Глинка Андрей Владимирович</v-card-title>
+    <v-card-subtitle>Форма проверки алгоритма</v-card-subtitle>
     <v-card-text>
       <v-form ref="form" v-model="valid" onSubmit="return false;">
         <v-text-field
@@ -10,8 +10,8 @@
             @input="result = ''"
             @keyup.enter="execute()"
             autofocus required
-            placeholder=""
-            label=""
+            placeholder="Введите только целое положительное число"
+            label="Введите число"
             class="pb-0"
         >
           <template v-slot:append-outer>
@@ -73,8 +73,16 @@ export default {
     ],
     examples: [
       {
-        input: '',
-        result: ''
+        input: '4',
+        result: '[1, 4]'
+      },
+      {
+        input: '9',
+        result: '[1, 4, 9]'
+      },
+      {
+        input: '16',
+        result: '[ 1, 4, 9, 16 ]'
       },
     ]
   }),
@@ -82,8 +90,8 @@ export default {
     execute() {
       if(this.$refs.form.validate()) {
         this.loading = true
-        this.axios.post('', {})
-          .then(r => {})
+        this.axios.get('state_on_switches', {params: {num: this.value}})
+          .then(r => this.result = r.data.message)
           .catch(e => this.result = 'Ошибка выполнения')
           .finally(() => this.loading = false)
       }
