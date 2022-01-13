2a4194e731c4b16f004964fbc59dec484bdfec6c
pushkin <maks.bur2809@gmail.com>
2022-01-13 12:48:01 +0300

    Задача на бэке

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,19 @@
+import math
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def get_sum_list(num: int) -> list:
+    cube = math.pow(num, 3)
+    result = []
+    a1 = int(cube/3)
+    for i in range(a1-2, a1+3, 2):
+        result.append(i)
+    return result
+
+
+@app.get("/sum_list")
+async def sum_list(num: int):
+    return {"message": get_sum_list(num)}
